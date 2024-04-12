0fff0608338e9ea68586ed2d8f5ced94beb20028
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2024-04-11 19:53:58 -0600

    remove

--- build/mac/nitro-app-dev.app/Contents/Resources/socket/worker.js
+++
@@ -1,6 +0,0 @@
-import { ServiceWorker } from './service-worker/instance.js'
-import { SharedWorker } from './internal/shared-worker.js'
-import { Worker } from './worker_threads.js'
-
-export { SharedWorker, ServiceWorker, Worker }
-export default Worker
