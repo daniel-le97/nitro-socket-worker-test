0b75779219c8a3b265328d73e62fd4736921044a
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2024-04-13 17:18:15 -0600

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Nitro starter
 
 Look at the [nitro quick start](https://nitro.unjs.io/guide#quick-start) to learn more how to get started.
+
+socketsupply/socket dev branch is needed for this
+
+currently to start with all of the correct typings please run
+
+```
+ssc build
+cp ./build/mac/nitro-app-dev.app/Contents/Resources/socket ./socket
+```
--- preset/entry.ts
+++ preset/entry.ts
@@ -1,41 +1,23 @@
 import "#internal/nitro/virtual/polyfills";
-import type { Context } from "socket:service-worker";
+import { nitroApp } from "nitropack/runtime/app";
+import os from "socket:os";
 // @ts-ignore
 import { isPublicAssetURL } from "#internal/nitro/virtual/public-assets";
+import type { Context } from "socket:service-worker";
 
-import { nitroApp } from "nitropack/runtime/app";
-// import fetcher from "socket:fetch";
-import os from "socket:os";
-// import * as ok from "socket:process";
-// @ts-ignore
-// export  { isPublicAssetURL } from "#internal/nitro/virtual/public-assets";
-
-// export const nitroApp = useNitroApp();
-// globalThis.process = ok
 export default {
   async fetch(request: Request, env = {}, ctx: Context) {
     ctx.data = os.uptime();
     const url = new URL(request.url);
-    // const req = new Request(request)
-    // const res = {
-    //   request,
-    //   env,
-    //   ctx,
-    //   os: os.platform(),
-    // }
-    // return new Response(JSON.stringify(res))
-    // const url = new URL(request.url);
     if (isPublicAssetURL(url.pathname) || url.pathname.includes("/_server/")) {
       return;
     }
 
-    let res = await handleEvent(url, request);
-    console.log({res, url, env,ctx});
-    // console.log(await res.text());
-    
-    return res
-    // const platform = os.platform();
-    // return new Response(nitroApp.h3App.options.debug + " " + platform);
+    const response = await handleEvent(url, request);
+    const clonedResponse = response.clone();
+    // { response: 'hello world!' }
+    console.log({response: await clonedResponse.text()});
+    return response
   },
 };
 
@@ -45,20 +27,6 @@ async function handleEvent(url: URL, request: Request) {
   if (request.body) {
     body = await request.arrayBuffer();
   }
-
-//  const res = await nitroApp.localCall({
-//     url: url.pathname + url.search,
-//     'headers': request.headers,
-//     method: request.method,
-//     'body': body,
-//     'protocol': url.protocol,
-//   })
-
-//   return new Response(res.body, {
-//     'headers': res.headers,
-//     'status': res.status,
-//     'statusText': res.statusText
-//   })
   return await nitroApp.localFetch(url.pathname + url.search, {
     host: url.hostname,
     protocol: url.protocol,
@@ -68,42 +36,3 @@ async function handleEvent(url: URL, request: Request) {
     body,
   });
 }
-// import type { Context } from "socket:service-worker/context";
-// import type {NitroApp} from "nitropack"
-
-// class MyWorker {
-//   nitroApp: NitroApp
-//   constructor() {
-//     this.nitroApp = useNitroApp();
-//   }
-
-//   async handleEvent(url: URL, event: any) {
-//     let body;
-//     if (event.request.body) {
-//       body = await event.request.arrayBuffer();
-//     }
-
-//     return await this.nitroApp.localFetch(url.pathname + url.search, {
-//       host: url.hostname,
-//       protocol: url.protocol,
-//       headers: event.request.headers,
-//       method: event.request.method,
-//       redirect: event.request.redirect,
-//       body,
-//     });
-//   }
-
-//   async handler (
-//     request: Request,
-//     env: Record<string, any>,
-//     ctx: Context
-//   ) {
-
-//     return await this.handleEvent(new URL(request.url), ctx)
-//   }
-
-// }
-
-// const main = new MyWorker()
-
-// export default main.handler
